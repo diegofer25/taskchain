99b6333702727973522f106dbd39552139a0fef5
Diego Lamarão <diego.lamarao92@gmail.com>
2025-04-21 21:04:24 -0300

    improve structure

--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-view-page">
+    <h1>Hello world</h1>
+  </div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style>
+.home-view-page {
+  width: 100%;
+}
+h1 {
+  @apply text-center heading-1;
+}
+</style>
