8fed27ac59e1e84cda9587a9e8cb9d44295ba8d4
Diego Lamarão <diego.lamarao92@gmail.com>
2025-04-26 17:22:28 -0300

    chore: update devDependencies in types package
    - Removed dependency on @taskchain/eslint-config
    - Added @x2d/eslint-config as a new dependency

--- packages/types/eslint.config.mjs
+++ packages/types/eslint.config.mjs
@@ -1,12 +1,12 @@
 import js from '@eslint/js';
-import taskchainEslintConfig from '@taskchain/eslint-config';
+import x2dEslintConfig from '@x2d/eslint-config';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
-  { files: ['**/*.{ts}'], extends: [taskchainEslintConfig] },
+  { files: ['**/*.{ts}'], extends: [x2dEslintConfig] },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.node } },
   { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
