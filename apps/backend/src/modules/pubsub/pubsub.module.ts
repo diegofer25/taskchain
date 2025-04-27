d25d70c72bef36dee06c60ddc3f9f90b1fa7225d
Diego Lamarão <diego.lamarao92@gmail.com>
2025-04-27 18:23:02 -0300

    BE(refactor): remove Global decorator from PubsubModule

--- apps/backend/src/modules/pubsub/pubsub.module.ts
+++ apps/backend/src/modules/pubsub/pubsub.module.ts
@@ -1,9 +1,9 @@
 import { WebPubSubServiceClient } from '@azure/web-pubsub';
-import { Global, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 export const PUBSUB_CLIENT = 'PUBSUB_CLIENT';
-@Global()
+
 @Module({
   imports: [ConfigModule],
   providers: [
